6c7351661f336ac82299ba2fc5b4d8bf1b6ffb00
Maria <maria4355@gmail.com>
2022-12-12 16:07:30 +0300

    New

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragment.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bFrag2.setOnClickListener { it: View!
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.place_holder, BlankFragment.newInstance())
+                .commit()
+        }
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, BlankFragment.newInstance())
+            .commit()
     }
 }
